9d343f9d46dc4fa69e8525a47d1510b161e503f2
iti-rina <itigecheva.irina@gmail.com>
2024-11-01 03:23:36 +0700

    add styles for App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,15 @@
 import { Typography } from 'antd';
 import { RepositoriesList } from '../features/repositoriesList/ui';
 import { Search } from '../widgets/Search/ui';
+import styles from './App.module.css';
 
 const App = () => {
   return (
-    <>
-      <Typography.Title level={1}>Search GitHub repositories</Typography.Title>
+    <div className={styles.container}>
+      <Typography.Title level={1} className={styles.title}>Search GitHub repositories</Typography.Title>
       <Search />
       <RepositoriesList />
-    </>
+    </div>
   )
 }
 
