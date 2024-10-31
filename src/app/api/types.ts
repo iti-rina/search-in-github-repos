73dbecca73bd75feaecc65df54c534932b840d89
iti-rina <itigecheva.irina@gmail.com>
2024-11-01 00:29:37 +0700

    fix: update types

--- src/app/api/types.ts
+++ src/app/api/types.ts
@@ -1,11 +1,11 @@
 type License = {
-  key: string;
-  name: string;
-  url: string;
-  spdx_id: string;
-  node_id: string;
-  html_url: string;
-}
+  key: string | null;
+  name: string | null;
+  url: string | null;
+  spdx_id: string | null;
+  node_id: string | null;
+  html_url: string | null;
+};
 
 type Owner = {
   login: string;
@@ -30,85 +30,95 @@ type Owner = {
 
 export type Repository = {
   id: number;
-  node_id: string;
+  node_id: string | null;
   name: string;
-  full_name: string;
+  full_name: string | null;
   owner: Owner;
-  private: boolean;
+  private: boolean | null;
   html_url: string;
-  description: string;
-  fork: boolean;
-  url: string;
-  created_at: string;
-  updated_at: string;
-  pushed_at: string;
-  homepage: string;
-  size: number;
+  description: string | null;
+  fork: boolean | null;
+  url: string | null;
+  created_at: string | null;
+  updated_at: string | null;
+  pushed_at: string | null;
+  homepage: string | null;
+  size: number | null;
   stargazers_count: number;
-  watchers_count: number;
-  language: string;
-  forks_count: number;
-  open_issues_count: number;
-  master_branch: string;
-  default_branch: string;
-  score: number;
-  archive_url: string;
-  assignees_url: string;
-  blobs_url: string;
-  branches_url: string;
-  collaborators_url: string;
-  comments_url: string;
-  commits_url: string;
-  compare_url: string;
-  contents_url: string;
-  contributors_url: string;
-  deployments_url: string;
-  downloads_url: string;
-  events_url: string;
-  forks_url: string;
-  git_commits_url: string;
-  git_refs_url: string;
-  git_tags_url: string;
-  git_url: string;
-  issue_comment_url: string;
-  issue_events_url: string;
-  issues_url: string;
-  keys_url: string;
-  labels_url: string;
-  languages_url: string;
-  merges_url: string;
-  milestones_url: string;
-  notifications_url: string;
-  pulls_url: string;
-  releases_url: string;
-  ssh_url: string;
-  stargazers_url: string;
-  statuses_url: string;
-  subscribers_url: string;
-  subscription_url: string;
-  tags_url: string;
-  teams_url: string;
-  trees_url: string;
-  clone_url: string;
-  mirror_url: string;
-  hooks_url: string;
-  svn_url: string;
-  forks: number;
-  open_issues: number;
-  watchers: number;
-  has_issues: boolean;
-  has_projects: boolean;
-  has_pages: boolean;
-  has_wiki: boolean;
-  has_downloads: boolean;
-  archived: boolean;
-  disabled: boolean;
-  visibility: string;
-  license: License;
+  watchers_count: number | null;
+  language: string | null;
+  forks_count: number | null;
+  open_issues_count: number | null;
+  master_branch: string | null;
+  default_branch: string | null;
+  score: number | null;
+  archive_url: string | null;
+  assignees_url: string | null;
+  blobs_url: string | null;
+  branches_url: string | null;
+  collaborators_url: string | null;
+  comments_url: string | null;
+  commits_url: string | null;
+  compare_url: string | null;
+  contents_url: string | null;
+  contributors_url: string | null;
+  deployments_url: string | null;
+  downloads_url: string | null;
+  events_url: string | null;
+  forks_url: string | null;
+  git_commits_url: string | null;
+  git_refs_url: string | null;
+  git_tags_url: string | null;
+  git_url: string | null;
+  issue_comment_url: string | null;
+  issue_events_url: string | null;
+  issues_url: string | null;
+  keys_url: string | null;
+  labels_url: string | null;
+  languages_url: string | null;
+  merges_url: string | null;
+  milestones_url: string | null;
+  notifications_url: string | null;
+  pulls_url: string | null;
+  releases_url: string | null;
+  ssh_url: string | null;
+  stargazers_url: string | null;
+  statuses_url: string | null;
+  subscribers_url: string | null;
+  subscription_url: string | null;
+  tags_url: string | null;
+  teams_url: string | null;
+  trees_url: string | null;
+  clone_url: string | null;
+  mirror_url: string | null;
+  hooks_url: string | null;
+  svn_url: string | null;
+  forks: number | null;
+  open_issues: number | null;
+  watchers: number | null;
+  has_issues: boolean | null;
+  has_projects: boolean | null;
+  has_pages: boolean | null;
+  has_wiki: boolean | null;
+  has_downloads: boolean | null;
+  archived: boolean | null;
+  disabled: boolean | null;
+  visibility: string | null;
+  license: Partial<License> | null;
 }
 
 export type SearchRepositoriesResponse = {
   total_count: number;
   incomplete_results: boolean;
   items: Repository[];
+}
+
+export type SimplifiedRepository = {
+  id: number,
+  name: string,
+  html_url: string,
+  stargazers_count: number,
+  owner_login: string,
+  owner_avatar_url: string,
+  repository_language: string | null
 }
