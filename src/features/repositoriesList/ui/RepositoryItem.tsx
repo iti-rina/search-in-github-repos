33f4078791179dc9c2daf290a45468f1f5d2f0d4
iti-rina <itigecheva.irina@gmail.com>
2024-11-01 00:43:51 +0700

    feat: add EditRepositoryModal to item component

--- src/features/repositoriesList/ui/RepositoryItem.tsx
+++ src/features/repositoriesList/ui/RepositoryItem.tsx
@@ -4,13 +4,26 @@ import type { SimplifiedRepository} from '../../../app/api';
 import { FC, useState } from 'react';
 import { IconText } from '../../../shared/ui';
 import { observer } from 'mobx-react-lite';
+import EditRepositoryModal from '../../EditRepository/ui/EditRepositoryModal';
 
 type RepositoryProps = {
   repository: SimplifiedRepository
 }
 
 const RepositoryItem: FC<RepositoryProps> = observer(({ repository }) => {
+  const [open, setOpen] = useState(false);
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setOpen(false);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
   const tag = repository.repository_language ? <Tag bordered={false} color='cyan'>{repository.repository_language}</Tag> : null;
+  
   return (
     <Badge.Ribbon text={`Owner: ${repository.owner_login}`}>
       <List.Item
@@ -23,6 +36,10 @@ const RepositoryItem: FC<RepositoryProps> = observer(({ repository }) => {
             title={<p>{repository?.name}</p>}
             description={<a href={repository?.html_url} target='_blank'>{repository?.html_url}</a>}
           />
+          <Button type="primary" onClick={showModal}>
+            Edit
+          </Button>
+          <EditRepositoryModal isOpen={open} onCancel={handleCancel} onOk={handleOk} repositoryId={repository.id}/>
         </List.Item>
     </Badge.Ribbon>
   );
